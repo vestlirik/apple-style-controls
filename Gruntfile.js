9cd47ff9a3ff2d49d40621e9dc78329d64f520dc
Victor Kiriltsev <vestlirik@gmail.com>
2017-04-01 04:17:36 +0300

    Added grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+/*global module,require*/
+module.exports = function (grunt) {
+
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        cssmin: {
+            build: {
+                files: [
+                    {
+                        src: 'dest/styles.css',
+                        dest: 'dest/styles.css'
+                    }
+                ]
+            }
+        },
+        processhtml: {
+            dist: {
+                files: {
+                    'dest/index.html': ['src/index.html']
+                }
+            }
+        },
+        sass: {
+            dist: {
+                files: {
+                    'dest/styles.css': 'src/styles.scss'
+                }
+            }
+        },
+        autoprefixer:{
+            dist:{
+                files:{
+                    'dest/styles.css':'dest/styles.css'
+                }
+            }
+        },
+        copy: {
+            build: {
+                files: [
+                    {
+                        cwd: 'src',
+                        src: '*.woff',
+                        dest: 'dest/',
+                        expand: true
+                    }]
+            }
+        },
+        uglify: {
+            build: {
+                files: {
+                    'dest/code.js': ['src/edit-menu/code.js']
+                }
+            }
+        }
+    });
+
+
+
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+
+    grunt.registerTask('default', [
+        'sass',
+        'autoprefixer',
+        'cssmin',
+        'processhtml',
+        'copy',
+        'uglify'
+    ]);
+};
