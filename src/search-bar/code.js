10c4956de7197bdea8e95033dd58cdeac4fba7b1
Viktor Kyryltsev <viktor.kyryltsev@blackthorn-vision.com>
2017-08-23 03:44:24 +0300

    Improved using search control

--- src/search-bar/code.js
+++ src/search-bar/code.js
@@ -9,9 +9,26 @@
         });
     }
 
+    function createSearchControl(parent) {
+        var searchIcon = eDOM.el('.search');
+        searchIcon.appendChild(eDOM.el('.search__circle'));
+        searchIcon.appendChild(eDOM.el('.search__rectangle'));
+        var searchInputEl = eDOM.el('input.asc.asc-search-input');
+        searchInputEl.setAttribute('placeholder', 'Search');
+        var cancel = eDOM.el('button.asc.asc-search-cancel');
+        cancel.innerText = "Cancel";
+        parent.appendChild(searchIcon);
+        parent.appendChild(searchInputEl);
+        parent.appendChild(cancel);
+    }
+
     document.addEventListener('addedNode', function (e) {
-        if (e.detail.classList.contains('asc-search-input')) {
-            applySearchInput(e.detail);
+        if (e.detail.classList.contains('asc-search-bar')) {
+            createSearchControl(e.detail);
+        } else {
+            if (e.detail.classList.contains('asc-search-input')) {
+                applySearchInput(e.detail);
+            }
         }
     });
 
