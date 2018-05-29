89a3ab4b357d99ff85f245782add1c81e0bb2c13
Viktor Kyryltsev <viktor.kyryltsev@blackthorn-vision.com>
2018-05-30 02:32:54 +0300

    Fixed adding icons to input

--- src/input/code.js
+++ src/input/code.js
@@ -1,23 +1,16 @@
 asc.component('input', function () {
-    this.init = function (input) {
+    this.afterInit = function (input) {
         if (!input.parentNode.classList.contains('input-container')) {
             var inputDiv = eDOM.el('.input-container.asc');
             input.parentNode.insertBefore(inputDiv, input.nextSibling);
             inputDiv.appendChild(input);
             var label = eDOM.el('label.asc.asc-eraser');
             inputDiv.appendChild(label);
-        }
-    };
-    this.afterInit = function (input) {
-        var icon = input.attributes["icon"];
-        if (icon) {
-            var iconDiv = eDOM.el('span.fa.' + icon.value);
-            iconDiv.style.position = "absolute";
-            iconDiv.style.marginTop = "10px";
-            iconDiv.style.marginLeft = "5px";
-            iconDiv.style.color = "#B3B3B3";
-            input.style.textIndent = "15px";
-            input.parentNode.insertBefore(iconDiv, input);
+            var icon = input.attributes["icon"];
+            if (icon) {
+                var iconDiv = eDOM.el('span.fa.' + icon.value);
+                input.parentNode.insertBefore(iconDiv, input);
+            }
         }
     }
 });
